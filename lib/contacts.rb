f2541013e3bd11f4129a74f54ad4afe87cbbf56b
William Mittard <billymittard@ymail.com>
2020-04-17 20:38:26 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,6 +15,6 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-contacts.each do |person, contact_details_hash|
-  binding.pry
-end
+contacts.each do |person, contact_details_hash| 
+end 
+ 
