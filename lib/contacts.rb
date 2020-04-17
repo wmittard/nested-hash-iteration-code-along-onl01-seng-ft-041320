703a37246b9b4698a58654db7028b903f59f212f
William Mittard <billymittard@ymail.com>
2020-04-17 20:54:37 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute,data|
-      binding.pry
+      if 
     end
   end
 end 
